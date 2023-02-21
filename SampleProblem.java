987b27822e35f7269ed55c295389b39b9d933b0d
AnkurDev <ankurtomar8@outlook.com>
2023-02-21 09:49:43 +0530

    Scanner class created

--- SampleProblem.java
+++ SampleProblem.java
@@ -1,5 +1,10 @@
+import java.lang.util.*;
+
+
 class HelloWorld {
     public static void main(String[] args) {
+	Scanner sc = new Scanner();
+	int a = sc.nextInt();
         System.out.println("Hello, World!"); 
     }
 }
