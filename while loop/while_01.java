465663af15c35bf63ec8dd522a7c57754b98b738
AnkurDev <ankurtomar8@outlook.com>
2023-03-03 20:26:46 +0530

    Added loops problems

---
+++ while loop/while_01.java
@@ -0,0 +1,18 @@
+// WAP to print integers from 1 to 10 using loop
+
+
+import java.util.*;
+class Main{
+        public static void main(String args[])
+  {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int i = 1;
+    
+    while(i<=n)
+    {
+        System.out.println("Number : " + i);
+        i++;
+    }
+  }
+}
