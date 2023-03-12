57a80cfeabc7b3264ca967a96e2cd21cae799ad3
AnkurDev <ankurtomar8@outlook.com>
2023-03-12 18:10:30 +0530

    Perfect Sqaure added

---
+++ Integers Problems/Perfect_Squares.java
@@ -0,0 +1,22 @@
+//Take an integer N as input. Print multiples of 4 till N.
+
+
+import java.util.*;
+
+
+class Main {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for( int i = 1 ; i <= n ; i++)
+        {
+            if (i > 0){
+              System.out.println("Perfect Square"+i+" is : "+(i*i));
+            }
+
+        }
+    }
+}
