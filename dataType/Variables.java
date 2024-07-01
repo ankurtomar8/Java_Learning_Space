b68051c1315aca205282ba5e85faa73d12c80749
AnkurDev <ankurtomar8@outlook.com>
2024-07-01 08:00:54 +0530

    Added Variables

---
+++ dataType/Variables.java
@@ -0,0 +1,22 @@
+// Variables types
+
+import java.util.*;
+
+
+class Main {
+    public static void main(String args[])
+    {
+      byte b = 127;
+      short s = 300;
+      int i = 100;
+      float f=25.f;
+
+      char c='A';
+
+      System.out.println(b);
+      System.out.println(s);
+      System.out.println(i);
+      System.out.println(f);
+
+    }
+}
